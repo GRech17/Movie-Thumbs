41a1e67c1908fa9b4d33eabe0ea42007bf8d40b7
Juan Chavez <jmchv2001@gmail.com>
2021-03-28 15:12:13 -0700

    api

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       port: 3306,
       dialect: 'mysql',
