7e02649c08745f25457e57011ea0f0bb9d181ec0
jennifer gomez <jennifer23gomez7@gmail.com>
2021-04-09 16:02:52 -0700

    comments route working,

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,13 +1,9 @@
 const commentFormHandler = async function(event) {
-
   event.preventDefault();
-
   var url = window.location.pathname;
   var id = url.substring(url.lastIndexOf('/') + 1);
-
   const movie_id = id;
   const comment_text = document.querySelector('input[name="comment_text"]').value;
-
   if (comment_text) {
     await fetch('/api/comments/' + movie_id, {
       method: 'POST',
@@ -19,9 +15,7 @@ const commentFormHandler = async function(event) {
         'Content-Type': 'application/json'
       }
     });
-
     document.location.reload();
   }
 };
-
 document.querySelector('.frmComment').addEventListener('submit', commentFormHandler);
